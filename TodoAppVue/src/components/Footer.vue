31a0dbc67f5e63ea132a50e14488d786bff8d472
Justice <ezejustice112@gmail.com>
2025-04-13 13:53:13 +0100

    first commit

---
+++ TodoAppVue/src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="bg-slate-900 text-center p-4 mt-50 text-blue-100 " >
+    <p>&copy; 2025 Justice Todo App. All rights reserved.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+</style>
---
+++ TodoAppVue/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="ho">
+      <div class="bg-slate-900 text-center mt-30 p-8 rounded-lg shadow-md mx-4 mt-6 md:mx-auto md:max-w-2xl">
+        <h1 class="todo text-3xl font-bold text-green-300">Welcome to My To-Do App!</h1>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    name: "Home",
+  };
+  </script>
