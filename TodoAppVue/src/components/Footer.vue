6d7bf62fb56df291b9c84e9c327086ee5b07b9ef
Justice <ezejustice112@gmail.com>
2025-04-25 20:17:58 +0100

    updated

--- TodoAppVue/src/components/Footer.vue
+++ TodoAppVue/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="bg-slate-900 text-center p-4 mt-50 text-blue-100 " >
-    <p>&copy; 2025 Justice Todo App. All rights reserved.</p>
+    <p>&copy; 2025 Ezebuike Justice Todo App. All rights reserved.</p>
   </footer>
 </template>
 
