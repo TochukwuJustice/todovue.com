7c72f18449d9bb55d64c7302d49cbe60d5d5052e
Justice <ezejustice112@gmail.com>
2025-04-26 18:22:24 +0100

    updates viteconfig

--- TodoAppVue/vite.config.js
+++ TodoAppVue/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/todovue.com/',
+  base: '/',
   plugins: [vue(), tailwindcss()],
 })
